f2799d382d82c3af146366d04d2f5b379a7b3d55
unknown <LYC@.(none)>
2015-12-04 12:46:10 +0800

    重复了

--- test/test/test/test/test/src/main/java/com/tt/repository/UserDao.java
+++
@@ -1,8 +0,0 @@
-package com.tt.repository;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-import com.tt.entity.User;
-
-public interface UserDao extends PagingAndSortingRepository<User, Long> {
-	User findByUsername(String username);
-	}
